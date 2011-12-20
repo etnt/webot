9b250267b7686ae37be2a39a7b9476a5627f2300
Torbjorn Tornkvist <tobbe@klarna.com>
2011-12-20 10:34:56 +0100

    Adding status switch to start script

--- start.sh
+++ start.sh
@@ -5,12 +5,18 @@ NAME=webot
 NODE=${NAME}@`hostname -s`
 
 if [ "$1" == "-i" ]; then
-  exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot
+    exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot
 elif [ "$1" == "-e" ]; then
-  exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin
+    exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin
 elif [ "$1" == "-d" ]; then
-  erl_call -n ${NODE} -q
+    erl_call -n ${NODE} -q
+elif [ "$1" == "-s" ]; then
+    STATUS=`erl_call -n ${NODE} -a 'erlang whereis [webot_sup]'`
+    if [ ${STATUS} != "undefined" ]; then
+	echo "${NAME} is running."
+    else
+	echo "${NAME} is not running."
+    fi
 else
   exec erl -sname webot -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot -detached
 fi
-
