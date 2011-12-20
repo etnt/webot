9b66702c5ffc9a3c3f6a0ff51619dc7a03686f77
Torbjorn Tornkvist <tobbe@klarna.com>
2011-12-20 10:39:52 +0100

    Using '=' as equal...grr...

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ cd `dirname $0`
 NAME=webot
 NODE=${NAME}@`hostname -s`
 
-if [ "$1" == "-i" ]; then
+if [ "$1" = "-i" ]; then
     exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot
-elif [ "$1" == "-e" ]; then
+elif [ "$1" = "-e" ]; then
     exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin
-elif [ "$1" == "-d" ]; then
+elif [ "$1" = "-d" ]; then
     erl_call -n ${NODE} -q
-elif [ "$1" == "-s" ]; then
+elif [ "$1" = "-s" ]; then
     STATUS=`erl_call -n ${NODE} -a 'erlang whereis [webot_sup]'`
     if [ ${STATUS} != "undefined" ]; then
 	echo "${NAME} is running."
