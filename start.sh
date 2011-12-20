7e9de8e5fc8d059642013d86ac2157c900cfd9c2
Torbjorn Tornkvist <tobbe@klarna.com>
2011-12-20 09:41:59 +0100

    Adding Debian start script

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd `dirname $0`
 
+NAME=webot
+NODE=${NAME}@`hostname -s`
+
 if [ "$1" == "-i" ]; then
-  exec erl -sname webot -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot
+  exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot
 elif [ "$1" == "-e" ]; then
-  exec erl -sname webot -pa $PWD/ebin $PWD/deps/*/ebin
+  exec erl -sname ${NAME} -pa $PWD/ebin $PWD/deps/*/ebin
+elif [ "$1" == "-d" ]; then
+  erl_call -n ${NODE} -q
 else
   exec erl -sname webot -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s webot -detached
 fi
